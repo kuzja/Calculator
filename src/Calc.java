3d701e6a979e87eae0011c9cbd81d356472d643e
Eugene <kuzmenko.evg@gmail.com>
2017-01-30 17:20:33 +0200

    Change RegEXP

--- src/Calc.java
+++ src/Calc.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,19 +12,17 @@ public class Calc {
 
     public long getResult() {
 
-        String pattern = "(\\(^-)?\\d+";
+        String pattern = "(^-)?\\d+";
 
         Pattern patternNumbers = Pattern.compile(pattern);
         Matcher matcher = patternNumbers.matcher(clearString);
 
         ArrayList<Long> numbers = new ArrayList<Long>();
 
-        while (matcher.find()){
+        while (matcher.find()) {
             numbers.add(Long.parseLong(matcher.group()));
-            System.out.println(matcher.group());
         }
 
-        pattern = "(^\\-)?\\d+";
         Pattern patternOperations = Pattern.compile(pattern);
         String stringOperations[] = patternOperations.split(clearString);
 
@@ -36,9 +33,9 @@ public class Calc {
         for (int i = 1; i < stringOperations.length; i++) {
             op = stringOperations[i];
             if (op.equals(Operation.Add.toString())) {
-                numbers.set(i, Operation.Add.action(numbers.get(i-1), numbers.get(i)));
+                numbers.set(i, Operation.Add.action(numbers.get(i - 1), numbers.get(i)));
             } else {
-                numbers.set(i, Operation.Sub.action(numbers.get(i-1), numbers.get(i)));
+                numbers.set(i, Operation.Sub.action(numbers.get(i - 1), numbers.get(i)));
             }
 
             result = numbers.get(i);
